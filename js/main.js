f97195873694521603929a14074f1d78d077c037
Facu Vera <fakundoo2010@gmail.com>
2024-03-23 13:06:40 -0300

    agrego function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-let nombre = prompt("ingrese un nombre de un alumno");
-
+let nombre;
 let materias = 4
 let contador = materias;
 let notafinal = 0;
 let aprobo;
+
+saludar();
+function saludar(){
+    nombre = prompt("Hola, ingrese un nombre de un alumno");
+}
 
 while(contador > 0){
     let nota = parseInt(prompt("ingrese una nota del 1 al 10 para la materia"));
